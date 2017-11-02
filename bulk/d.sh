fc3696b2978c4ff31c1a1820cdfdd913fd7b613d
childsb <bchilds@redhat.com>
2017-11-01 23:33:26 -0400

    update delete script

--- bulk/d.sh
+++ bulk/d.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 DEFAULT_NAMESPACE=fatapp
-for number in {1..2} 
+for number in {2..100} 
 do
   PROJECT_NAME=${DEFAULT_NAMESPACE}-${number}
   echo "deleting namespace: $PROJECT_NAME"
+
+  oc project $PROJECT_NAME
+  oc delete pvc --all
   oc delete ns $PROJECT_NAME
+
 done
-  oc delete -f ./helloworld.yml
+
+
+oc delete -f ./helloworld.yml
